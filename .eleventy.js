8baf9634dd8ad3e00a429947abb545a8b667f93b
Tyler Schreiber <tschreiber12@georgefox.edu>
2024-05-13 23:36:06 -0700

    Fixed link list issues; created linkList shortcode; removed old Quartz sass, etc.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const { parse } = require('csv-parse/sync');
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css/base.css");
+    eleventyConfig.addPassthroughCopy("src/css/*.css");
     eleventyConfig.addDataExtension("csv", (contents) => {
         const records = parse(contents, {
           columns: true,
@@ -12,8 +12,20 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
         files: './_site/css/**/*.css'
       });
+
+    eleventyConfig.addShortcode("linkList", function(list, pages) {
+      var pageNames = pages.map(page => page.data.title);
+      return `<ul>
+        `  + list.map(item => pageNames.includes(item) ? 
+            `<li><a href="${pages.filter(page => page.data.title === item)[0].url}">${item}</a></li>` :
+            `<li>${item}</li>`).join(`
+            `) + `
+          </ul>`
+    });
+
     return {
         passthroughFileCopy: true,
+        markdownTemplateEngine: "njk",
         dir: {      
             input: "src",      
             includes: "_includes",      
