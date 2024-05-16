d38048f9c7ffa486e306eb761abe22e167eb5e46
Tyler Schreiber <tschreiber12@georgefox.edu>
2024-05-15 23:33:46 -0700

    Ported over most of the old site (minus individual tracks) - markdown can now be embedded into HTML and rendered correctly (!).

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 const { parse } = require('csv-parse/sync');
+const eleventyNavigation = require('@11ty/eleventy-navigation');
+const markdownIt = require("markdown-it");
+
+function compareAlphabetically(a, b) {
+  if (a.data.title < b.data.title) {
+    return -1;
+  }
+  if (a.data.title > b.data.title) {
+    return 1;
+  }
+  return 0;
+}
 
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigation);
     eleventyConfig.addPassthroughCopy("src/css/*.css");
     eleventyConfig.addDataExtension("csv", (contents) => {
         const records = parse(contents, {
@@ -13,6 +26,22 @@ module.exports = function(eleventyConfig) {
         files: './_site/css/**/*.css'
       });
 
+    eleventyConfig.addCollection("alphabeticalArtist", function(collection) {
+      return collection.getFilteredByTag('artist').sort(compareAlphabetically);
+    });
+    eleventyConfig.addCollection("alphabeticalGenre", function(collection) {
+      return collection.getFilteredByTag('genre').sort(compareAlphabetically);
+    });
+    eleventyConfig.addCollection("alphabeticalRelease", function(collection) {
+      return collection.getFilteredByTag('release').sort(compareAlphabetically);
+    });
+
+    eleventyConfig.addTransform("links", function(content) {
+      const path = String(this.page.outputPath);
+      if (!(path.endsWith(".html") || path.endsWith(".md"))) return content;
+      return content.replace(/\[\[(.*?)\|(.*?)\]\]/gm, `<a href="/$1">$2</a>`)
+                    .replace(/\[\[(.*?)\]\]/gm, `<a href="/$1">$1</a>`);
+    });
     eleventyConfig.addShortcode("linkList", function(list, pages) {
       var pageNames = pages.map(page => page.data.title);
       return `<ul>
@@ -22,6 +51,11 @@ module.exports = function(eleventyConfig) {
             `) + `
           </ul>`
     });
+    eleventyConfig.addShortcode("markdown", (content) => {
+      md = new markdownIt();
+      return md.render(content);
+    });
+    eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
 
     return {
         passthroughFileCopy: true,
