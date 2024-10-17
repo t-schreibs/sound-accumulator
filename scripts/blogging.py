d5e87fb8ed9aa0b9f8be873b2b61b33e816d7e87
Tyler Schreiber <tylereschreiber@gmail.com>
2024-10-17 01:20:18 -0700

    Improved output of add-blog-post script - artist list now includes descriptions.

--- scripts/blogging.py
+++ scripts/blogging.py
@@ -22,18 +22,18 @@ def add_artist_list(post, artists):
     with HeaderSubLevel(post):
         post.add(Header("Artists"))
         post.add('');
-        links = [f'[[artists/{artist}|{artist}]]' for artist in artists];
+        links = [f'[[artists/{artist[0]}|{artist[0]}]] - {artist[5]}' for artist in artists];
         post.add(UnorderedList(links));
 
 def get_filepath(post_name, include_extension=True):
     return Path.joinpath(BLOG_PATH, f'{post_name}{'.md' if include_extension else ''}');
 
 def get_artists_to_include_in_next_post():
-    artists = [artist[0] for artist in music_entries.get('artist')];
+    artists = music_entries.get('artist');
     artists_to_exclude = [];
     for filename in os.listdir(BLOG_PATH):
         with open(get_filepath(filename, False), "r") as file:
             post = frontmatter.load(file);
             if ('artists' in post.keys()):
                 artists_to_exclude.extend(post['artists']);
-    return [artist for artist in artists if artist not in artists_to_exclude];
+    return [artist for artist in artists if artist[0] not in artists_to_exclude];
